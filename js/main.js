ca852c763ace68d568cdda8f9636874c95d24d9d
anukul sahu <anukulsahu01@gmail.com>
2022-08-10 00:26:03 +0530

    first

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//Initialize and add the map
+function initMap() {
+     // Your location
+     const loc = { lat: 23.256721421493623, lng: 77.47784132059488 };
+     // Centered map on location
+     const map = new google.maps.Map(document.querySelector('.map')
+          , {
+               zoom: 15,
+               center: loc
+          });
+     // The marker, positioned at location
+     const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+
+// Sticky menu background
+window.addEventListener('scroll', function () {
+     if (window.scrollY > 150) {
+          document.querySelector('#navbar').style.opacity = 0.9;
+     } else {
+          document.querySelector('#navbar').style.opacity = 1;
+     }
+});
+
+
+// Smooth Scrolling code from JQuery CDN
+$('#navbar a, .btn').on('click', function (event) {
+     if (this.hash !== '') {
+          event.preventDefault();
+
+          const hash = this.hash;
+
+          $('html, body').animate(
+               {
+                    scrollTop: $(hash).offset().top - 100
+               },
+               800
+          );
+     }
+});
